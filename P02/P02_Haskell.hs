a077878e93d8c671b052bf1e2b3431bd99680b2b
xiro-bit <xirone@ciencias.unam.mx>
2024-09-27 18:47:10 -0600

    modificando Practica02

--- P02/P02_Haskell.hs
+++ P02/P02_Haskell.hs
@@ -1,3 +1,4 @@
+
 --------------- Listas y recursión ---------------
 
 longitud :: [a] -> Int
@@ -6,29 +7,33 @@ longitud(x:xs)= 1 + longitud(xs)
 
 sumaLista :: Num a => [a] -> a
 sumaLista []=0
-sumaLista (x:xs) = (x + sumaLista xs)
+sumaLista (x:xs) = x + sumaLista xs
 
 agregaElemento :: [a] -> a -> Bool -> [a]
-agregaElemento [] x condicion = if True
-                                then x :[]
+agregaElemento xs x condicion = if condicion
+                                then x :xs
                                 else 
-                                   []++[x]  
+                                   xs++[x] 
 
 maximoLista :: (Num a, Ord a) => [a] -> a
-maximoLista _ = undefined
+maximoLista [x] = x
+maximoLista (x:xs) = if x > maximoLista xs
+                     then x
+                     else maximoLista xs
 
 indice :: [a] -> Int -> a
-indice[x]= 0
-indice(x:xs)= if indice ==0
+indice[x] 0 = x
+indice(x:xs) i = if i == 0
                 then x
-                else indice xs (indice - 1)
+                else indice xs (i -1)
 --------------- Listas por comprehensión ---------------
 
 divisores :: Int -> [Int]
-divisores _ = undefined
+divisores n = [a | a <- [1..n], n `mod` a == 0]
 
 conjunto :: Eq a => [a] -> [a]
-conjunto _ = undefined
+conjunto []=[]
+conjunto (x:xs)= x:conjunto[y|y <- xs, y/=x]
 
 numerosPares :: [Int] -> [Int]
-numerosPares _ = undefined
+numerosPares (x:xs) = [x | x <- (x:xs), x `mod` 2 == 0]
