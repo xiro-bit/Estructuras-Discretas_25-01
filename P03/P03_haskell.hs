f777ec881ada30a12ed088d431016b69fb80e312
xiro-bit <xirone@ciencias.unam.mx>
2024-10-04 16:23:41 -0600

    Subiendo Practica 3 en Haskell

---
+++ P03/P03_haskell.hs
@@ -0,0 +1,51 @@
+data List a = Void | Node a (List a) deriving Show
+
+longitud :: List a -> Int
+longitud Void = 0
+longitud (Node a xs) = 1 + longitud xs
+
+estaContenido :: Eq a => List a -> a -> Bool
+estaContenido Void a= False
+estaContenido (Node x xs) a = if x==a
+                            then True
+                            else estaContenido(xs) a
+
+
+convertirAEstructura :: [a] -> List a
+convertirAEstructura[]= Void
+convertirAEstructura [x]= (Node x (Void))
+convertirAEstructura (x:xs) = (Node x (convertirAEstructura xs))
+
+convertirALista :: List a -> [a]
+convertirALista Void = []
+convertirALista (Node a Void) = [a]
+convertirALista (Node a xs)= a:(convertirALista xs)
+                            
+conjunto :: Eq a => List a -> List a
+conjunto Void= Void
+conjunto (Node a Void)= Node a Void
+conjunto (Node a xs) = if estaContenido xs a
+                        then conjunto xs
+                        else Node a (conjunto xs)
+
+
+eliminarIndice :: List a -> Int -> List a
+eliminarIndice (Node a Void) 0 = Void
+eliminarIndice (Node a xs)   0 = xs
+eliminarIndice (Node a xs)   x = if (x >= 0 && x < longitud (Node a xs)) 
+                                then Node a (eliminarIndice xs (x-1))  
+                                else error "indice fuera del rango permitido"                          
+                                
+
+insertarIndice :: List a -> Int -> a -> List a
+insertarIndice Void 0 valor= Node valor (Void)
+insertarIndice (Node x xs) 0 valor= Node valor (Node x xs) 
+insertarIndice (Node x xs) indice valor = if (indice>= 0 && indice <= longitud (Node x xs))
+                                then Node x (insertarIndice xs (indice-1) valor) 
+                                else error "indice fuera del rango permitido" 
+
+recorrerLista :: List a -> Int -> List a
+recorrerLista Void 0 = Void
+recorrerLista (Node a xs) 0 = Node a xs
+recorrerLista (Node a xs) indice = recorrerLista (insertarIndice xs (longitud xs) a) (indice-1)
+                            
