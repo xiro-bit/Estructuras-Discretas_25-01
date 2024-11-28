eddaea948789e62db729ca3fedf339026fa88fd1
xiro-bit <xirone@ciencias.unam.mx>
2024-11-28 15:09:53 -0600

    Se modificaron funciones 3 a 8

--- P03/P03_haskell.hs
+++ P03/P03_haskell.hs
@@ -13,39 +13,41 @@ estaContenido (Node x xs) a = if x==a
 
 convertirAEstructura :: [a] -> List a
 convertirAEstructura[]= Void
-convertirAEstructura [x]= (Node x (Void))
 convertirAEstructura (x:xs) = (Node x (convertirAEstructura xs))
 
 convertirALista :: List a -> [a]
 convertirALista Void = []
-convertirALista (Node a Void) = [a]
 convertirALista (Node a xs)= a:(convertirALista xs)
                             
 conjunto :: Eq a => List a -> List a
 conjunto Void= Void
-conjunto (Node a Void)= Node a Void
 conjunto (Node a xs) = if estaContenido xs a
                         then conjunto xs
                         else Node a (conjunto xs)
 
 
 eliminarIndice :: List a -> Int -> List a
-eliminarIndice (Node a Void) 0 = Void
-eliminarIndice (Node a xs)   0 = xs
-eliminarIndice (Node a xs)   x = if (x >= 0 && x < longitud (Node a xs)) 
-                                then Node a (eliminarIndice xs (x-1))  
-                                else error "indice fuera del rango permitido"                          
+eliminarIndice Void _ = Void
+eliminarIndice (Node _ xs) 0 = xs
+eliminarIndice (Node a xs) x =  if x > 0 && x < longitud (Node a xs)
+                                 then Node a (eliminarIndice xs (x - 1))
+                                 else error "índice fuera del rango permitido"
+                         
                                 
 
 insertarIndice :: List a -> Int -> a -> List a
-insertarIndice Void 0 valor= Node valor (Void)
-insertarIndice (Node x xs) 0 valor= Node valor (Node x xs) 
-insertarIndice (Node x xs) indice valor = if (indice>= 0 && indice <= longitud (Node x xs))
-                                then Node x (insertarIndice xs (indice-1) valor) 
-                                else error "indice fuera del rango permitido" 
+insertarIndice Void 0 valor = Node valor Void
+insertarIndice Void _ valor = error "índice fuera del rango permitido" 
+insertarIndice (Node x xs) 0 valor = Node valor (Node x xs)
+insertarIndice (Node x xs) indice valor = if indice > 0 && indice <= longitud (Node x xs)
+                                             then Node x (insertarIndice xs (indice - 1) valor)
+                                             else error "índice fuera del rango permitido"
+ 
 
 recorrerLista :: List a -> Int -> List a
-recorrerLista Void 0 = Void
+recorrerLista Void _ = Void
 recorrerLista (Node a xs) 0 = Node a xs
-recorrerLista (Node a xs) indice = recorrerLista (insertarIndice xs (longitud xs) a) (indice-1)
+recorrerLista (Node a xs) indice = recorrerLista (insertarIndice xs (longitud xs) a) (indice - 1)
+
                             
+ 
